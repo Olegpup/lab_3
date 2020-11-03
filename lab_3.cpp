9da0056a9b91e5d48a8eee5b4f46f18f289f173f
Olegpup <71699911+Olegpup@users.noreply.github.com>
2020-11-03 13:23:27 +0200

    Update lab_3.cpp

--- lab_3.cpp
+++ lab_3.cpp
@@ -4,16 +4,14 @@ using namespace std;
 #define pi 3.1415926536
 
 int main() {
-    double x, e, a = 0, sum = 0, b = pi;
+    double x = 0, e, a = 0, sum = 0, b = pi;
     int i = 0;
     cout << "Enter precision:\n ";
     cin >> e;
-    while (1) {
-        x = a + (e*i);
-        sum += (log(2 + sin(x))*e);
-        if (x > (b - e)) {
-            break;
-        }
+
+    while (x <= (b - e)) {
+        x = a + (e * i);
+        sum += (log(2 + sin(x)) * e);
         i += 1;
     }
     cout << "Answer: " << sum;
