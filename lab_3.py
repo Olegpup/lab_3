14ef187e32efb5df7c62cb8253df0a62e20f7b89
Olegpup <71699911+Olegpup@users.noreply.github.com>
2020-11-03 13:24:54 +0200

    Update lab_3.py

--- lab_3.py
+++ lab_3.py
@@ -3,11 +3,10 @@ a = 0
 b = math.pi
 i = 0
 sum = 0
+x = 0
 e = float(input("Enter precision: "))
-while True:
+while x <= (b - e):
     x = a + e*i
     sum += math.log(2 + math.sin(x))*e
-    if x > (b - e):
-        break
     i += 1
 print("Answer:",sum)
